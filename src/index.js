d36331e2c1872cdf0be5ebd6314076b675b0bdb5
Shivam Chaubey <shivamc36@gmail.com>
2023-06-26 22:17:25 +0530

    file and folder structre

--- src/index.js
+++ src/index.js
@@ -2,4 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import {BrowserRouter} from 'react-router-dom';
+
+ReactDOM.render(
+
+<BrowserRouter>
+
+<App />
+</BrowserRouter>,
+ document.getElementById("root"));
